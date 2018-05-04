c24bf8636a082921f7a9d32df60205a19522d9f8
Dr. Nikolaos Kazepis, PhD <nick.kazepis@gmail.com>
2018-05-04 13:39:33 +0300

    Upgrade for VisualStudio 2017 and format files that we are gonna use

--- Example3/Public/Js/echotest.js
+++ Example3/Public/Js/echotest.js
@@ -10,59 +10,59 @@ var url = "ws://localhost:4649/Echo";
 //var url = "wss://localhost:5963/Echo";
 var output;
 
-function init () {
-  output = document.getElementById ("output");
-  doWebSocket ();
+function init() {
+    output = document.getElementById("output");
+    doWebSocket();
 }
 
-function doWebSocket () {
-  websocket = new WebSocket (url);
+function doWebSocket() {
+    websocket = new WebSocket(url);
 
-  websocket.onopen = function (e) {
-    onOpen (e);
-  };
+    websocket.onopen = function (e) {
+        onOpen(e);
+    };
 
-  websocket.onmessage = function (e) {
-    onMessage (e);
-  };
+    websocket.onmessage = function (e) {
+        onMessage(e);
+    };
 
-  websocket.onerror = function (e) {
-    onError (e);
-  };
+    websocket.onerror = function (e) {
+        onError(e);
+    };
 
-  websocket.onclose = function (e) {
-    onClose (e);
-  };
+    websocket.onclose = function (e) {
+        onClose(e);
+    };
 }
 
-function onOpen (event) {
-  writeToScreen ("CONNECTED");
-  send ("WebSocket rocks");
+function onOpen(event) {
+    writeToScreen("CONNECTED");
+    send("WebSocket rocks");
 }
 
-function onMessage (event) {
-  writeToScreen ('<span style="color: blue;">RESPONSE: ' + event.data + '</span>');
-  websocket.close ();
+function onMessage(event) {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + event.data + '</span>');
+    websocket.close();
 }
 
-function onError (event) {
-  writeToScreen ('<span style="color: red;">ERROR: ' + event.data + '</span>');
+function onError(event) {
+    writeToScreen('<span style="color: red;">ERROR: ' + event.data + '</span>');
 }
 
-function onClose (event) {
-  writeToScreen ("DISCONNECTED");
+function onClose(event) {
+    writeToScreen("DISCONNECTED");
 }
 
-function send (message) {
-  writeToScreen ("SENT: " + message);
-  websocket.send (message);
+function send(message) {
+    writeToScreen("SENT: " + message);
+    websocket.send(message);
 }
 
-function writeToScreen (message) {
-  var pre = document.createElement ("p");
-  pre.style.wordWrap = "break-word";
-  pre.innerHTML = message;
-  output.appendChild (pre);
+function writeToScreen(message) {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
 }
 
-window.addEventListener ("load", init, false);
+window.addEventListener("load", init, false);
